b833d67bdb22e11603ebcafe17a44c78635dbea0
Eric Endsley <eric.endsley4@gmail.com>
2020-10-21 10:30:40 -0700

    rewrote madlib with our own story and inputs
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>
    co-authored-by: Randel Moore <randel.c.moore@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    const noun = $("input#noun").val();
+    const verb = $("input#verb").val();
+    const adjective = $("input#adjective").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    $(".noun").text(noun);
+    $(".verb").text(verb);
+    $(".adjective").text(adjective);
 
-    $("#story").show();
+    $('#story').show();
 
     event.preventDefault();
   });
 });
+
+
+
+
